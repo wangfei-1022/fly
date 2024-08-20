ab315b89eac7a1b3d6103d5c3c0979e5f22a0cb1
wangfei <927230462@qq.com>
2024-08-20 17:06:25 +0800

    fixed

--- ux/src/views/login/login.vue
+++ ux/src/views/login/login.vue
@@ -56,7 +56,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2023 oddfar.com All Rights Reserved.</span>
+      <span>Copyright © 2023 wangfei.com All Rights Reserved.</span>
     </div>
   </div>
 </template>
