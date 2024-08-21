bdeb973c5387692e7ac7a1edfc489997283ee7f6
wangfei <927230462@qq.com>
2024-08-21 11:08:19 +0800

    fixed

--- ux/src/views/login/login.vue
+++ ux/src/views/login/login.vue
@@ -1,59 +1,62 @@
 <template>
   <div class="login">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">WF管理系统</h3>
-      <el-form-item prop="username">
-        <el-input
-          v-model="loginForm.username"
-          type="text"
-          auto-complete="off"
-          placeholder="账号"
-        >
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input
-          v-model="loginForm.password"
-          type="password"
-          auto-complete="off"
-          placeholder="密码"
-          @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="code">
-        <el-input
-          v-model="loginForm.captcha"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 69%"
-          @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
-        </el-input>
-        <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
-        </div>
-      </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-      <el-form-item style="width:100%;">
-        <el-button
-          :loading="loading"
-          size="medium"
-          type="primary"
-          style="width:100%;"
-          @click.native.prevent="handleLogin"
-        >
-          <span v-if="!loading">登 录</span>
-          <span v-else>登 录 中...</span>
-        </el-button>
-        <div style="float: right;" v-if="register">
-          <router-link class="link-type" :to="'/register'">立即注册</router-link>
-        </div>
-      </el-form-item>
-    </el-form>
+    <div class="login-left"></div>
+    <div class="login-right">
+     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
+       <h3 class="title">WF</h3>
+       <el-form-item prop="username">
+         <el-input
+           v-model="loginForm.username"
+           type="text"
+           auto-complete="off"
+           placeholder="账号"
+         >
+           <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
+         </el-input>
+       </el-form-item>
+       <el-form-item prop="password">
+         <el-input
+           v-model="loginForm.password"
+           type="password"
+           auto-complete="off"
+           placeholder="密码"
+           @keyup.enter.native="handleLogin"
+         >
+           <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
+         </el-input>
+       </el-form-item>
+       <el-form-item prop="code">
+         <el-input
+           v-model="loginForm.captcha"
+           auto-complete="off"
+           placeholder="验证码"
+           style="width: 69%"
+           @keyup.enter.native="handleLogin"
+         >
+           <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
+         </el-input>
+         <div class="login-code">
+           <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+         </div>
+       </el-form-item>
+       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+       <el-form-item style="width:100%;">
+         <el-button
+           :loading="loading"
+           size="medium"
+           type="primary"
+           style="width:100%;"
+           @click.native.prevent="handleLogin"
+         >
+           <span v-if="!loading">登 录</span>
+           <span v-else>登 录 中...</span>
+         </el-button>
+         <div style="float: right;" v-if="register">
+           <router-link class="link-type" :to="'/register'">立即注册</router-link>
+         </div>
+       </el-form-item>
+     </el-form>
+    </div>
     <!--  底部  -->
     <div class="el-login-footer">
       <span>Copyright © 2023 wangfei.com All Rights Reserved.</span>
@@ -157,9 +160,22 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../../assets/images/login-background.jpg");
+
   background-size: cover;
+
+  .login-left{
+    height: 100%;
+    width: 68%;
+    background: linear-gradient(to right, #fff 0%, #d5e2f5 100%);
+    background-size: auto 100%;
+  }
+  .login-right{
+    width: 32%;
+    padding-top: 6%;
+    height: 100%;
+  }
 }
+
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
@@ -176,6 +192,13 @@ export default {
     input {
       height: 38px;
     }
+  }
+  .el-input__inner {
+    height: 40px;
+    background-color: #fff!important;
+    border: 0 none;
+    border-radius: 0;
+    border-bottom: 1px solid #e6e6e6 !important;
   }
   .input-icon {
     height: 39px;
@@ -212,6 +235,7 @@ export default {
 .login-code-img {
   height: 38px;
   border: 1px solid #eee;
-  border-radius: 5px;
+  border-radius: 2px;
+  width: 100%;
 }
 </style>
