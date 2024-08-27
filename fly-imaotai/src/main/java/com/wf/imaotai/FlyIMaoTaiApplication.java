7efc4db1a0d7190a95c814d380ea2352e6e7831f
wangfei <927230462@qq.com>
2024-08-27 15:20:47 +0800

    add redis

--- fly-imaotai/src/main/java/com/wf/imaotai/FlyIMaoTaiApplication.java
+++ fly-imaotai/src/main/java/com/wf/imaotai/FlyIMaoTaiApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.wf.imaotai", "com.wf.common" })
 @MapperScan(basePackages = "com.wf.imaotai.mapper")
 public class FlyIMaoTaiApplication {
 
